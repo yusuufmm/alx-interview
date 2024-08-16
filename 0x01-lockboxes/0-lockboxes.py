0868058e30b5dd1abbb7d83d8ef7165fa51c9522
Yusuf Muhammad Musa <yusuf2000mm@gmail.com>
2024-08-16 03:08:14 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# Solves lock boxes puzzle
+
+
+def look_next_opened_box(opened_boxes):
+    # Find next opened box and return its keys
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
+
+def canUnlockAll(boxes):
+    # Check if all boxes can be opened
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
+
+    aux = {}
+    while True:
+        if len(aux) == 0:
+            aux[0] = {'status': 'opened', 'keys': boxes[0]}
+
+        keys = look_next_opened_box(aux)
+        if keys:
+            for key in keys:
+                try:
+                    if aux.get(key) and aux.get(key).get('status') \
+                       == 'opened/checked':
+                        continue
+                    aux[key] = {'status': 'opened', 'keys': boxes[key]}
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in aux.values()]:
+            continue
+        elif len(aux) == len(boxes):
+            break
+        else:
+            return False
+
+    return len(aux) == len(boxes)
+
+
+def main():
+    # Entry point
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
