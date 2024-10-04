13724cd98d6716e702da2dbcf78bf4147ff58309
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2024-10-03 18:19:06 -0700

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
+""" Change comes from within """
 
 def makeChange(coins, total):
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    """
+    Returns: fewest number of coins needed to meet total """
     if not coins or coins is None:
         return -1
     if total <= 0:
